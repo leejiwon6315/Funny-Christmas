4114a3068350c1280d4d54de4fc1a77a127c74dc
leejiwon6315 <leejiwon6315@naver.com>
2020-12-24 01:26:02 +0900

    add style

---
+++ fcApp/public/javascripts/app.js
@@ -0,0 +1,7 @@
+let num0 = 0,
+  num1 = 0,
+  num2 = 0;
+
+const input0 = document.getElementById("input0");
+const input1 = document.getElementById("input1");
+const input2 = document.getElementById("input2");
